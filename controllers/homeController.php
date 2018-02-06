b7f5b83ef8fe2d66333b19df950aa2fbee3e011e
(Souza) <(souzaoe@hotmail.com)>
2018-02-06 16:45:34 -0200

    Finalização do controller

---
+++ controllers/homeController.php
@@ -0,0 +1,13 @@
+<?php
+class homeController {
+	// controlador da pagina inicial do sistema
+
+	public function index() {
+		echo 'Ola maluco'; 
+	}
+
+	public function teste() {
+		echo "Este é um teste!";
+	}
+}
+?>
